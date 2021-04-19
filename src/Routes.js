95d8e58329a031c1116fa92021417586681f7bcc
Juyoung Son <wndud0647@gmail.com>
2021-04-20 01:18:45 +0900

    Add: 로그인 컴포넌트 레이아웃, Input기능 완료

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Main from "./pages/Main/Main";
-import Login from "./pages/Login/Login";
-import Signup from "./pages/Signup/Signup";
-import Shoes from "./pages/Shoes/Shoes";
-import Detail from "./pages/Detail/Detail";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Main from './pages/Main/Main';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
+import Shoes from './pages/Shoes/Shoes';
+import Detail from './pages/Detail/Detail';
 
 class Routes extends React.Component {
   render() {
