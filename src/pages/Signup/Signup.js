a9a31a4390721d93932feec8802311e41132713b
Juyoung Son <wndud0647@gmail.com>
2021-04-19 21:57:11 +0900

    Modify: 회원가입 페이지 레이아웃 작업 중

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -14,12 +14,14 @@ class Signup extends Component {
       month: '',
       day: '',
       email: '',
+      emailDot: '',
       phoneNum1: '',
       phoneNum2: '',
       phoneNum3: '',
     },
     phoneNum: '',
     date: '',
+    email: '',
   };
 
   handleOnchange = e => {
@@ -35,15 +37,56 @@ class Signup extends Component {
       },
       () => {
         this.setState({
-          phoneNum: form.phoneNum1 + form.phoneNum2 + form.phoneNum3,
-          date: form.year + form.month + form.day,
+          ...this.state.phoneNum,
+          ...this.state.date,
+          phoneNum:
+            form.phoneNum1 + '-' + form.phoneNum2 + '-' + form.phoneNum3,
+          date: form.year + '-' + form.month + '-' + form.day,
+          email: form.email + '@' + form.emailDot,
         });
       }
     );
   };
 
+  // goTo = () => {
+  //   console.log(this.state.form);
+  //   fetch('http://10.58.56.241:8000/users/signup', {
+  //     method: 'POST',
+  //     Headers: { 'Content-Type': 'application/json' },
+  //     body: JSON.stringify({
+  //       account: this.state.account,
+  //       password: this.state.password,
+  //       name: this.state.name,
+  //       date: this.state.date,
+  //       phoneNum: this.state.phoneNum,
+  //       email: this.state.email,
+  //     }),
+  //   })
+  //     .then(res => res.json())
+  //     .then(res => {
+  //       if (res.token) {
+  //         localStorage.setItem('wtw-token', res.token);
+  //       }
+  //     });
+  // };
+
+  // componentDidMount() {
+  //   // const requestOption = {
+  //   //   method: 'POST',
+  //   //   Headers: { 'Content-Type': 'application/json' },
+  //   //   body: JSON.stringify({
+  //   //     account: this.state.account,
+  //   //     password: this.state.password,
+  //   //     name: this.state.name,
+  //   //     date: this.state.date,
+  //   //     phoneNum: this.state.phoneNum,
+  //   //     email: this.state.email,
+  //   //   }),
+  //   // };
+  // }
+
   render() {
-    console.log(this.state.form);
+    console.log(this.state.date);
     const { handleOnchange } = this;
     return (
       <div className="signUp">
@@ -60,13 +103,39 @@ class Signup extends Component {
             <div className="underBar"></div>
           </div>
         </div>
+        <div className="premiumWrapper">
+          <div className="premiumBox">
+            <h3 className="premiumTitle">F&F 통합회원 가입</h3>
+            <p className="premiumContent">
+              Discovery Expedition의 회원이 되시면 하나의 동일한 ID와 비밀번호로
+              Discovery Expedition은 물론, F&F의 패밀리 브랜드의 통합회원으로
+              다양한 서비스를 안전하고 편리하게 이용하실 수 있습니다.
+            </p>
+            <p className="premiumContent">
+              <strong className="premiumContentBold">F&F Family Brand </strong>
+              <span className="premiumArrow">&gt;</span>Discovery Expedition,
+              MLB, MLB KIDS, COLLECTED
+            </p>
+          </div>
+        </div>
+
         <form className="signUpForm">
+          <div className="signUpText">
+            <h3>가입정보입력</h3>
+            <p>
+              <span className="nessaryIcon">*</span>
+              필수입력
+            </p>
+          </div>
           <div className="signUpItemBox">
-            <div className="signUpItem">아이디*</div>
+            <div className="signUpItem">
+              아이디<span className="nessaryIcon">*</span>
+            </div>
             <div className="signUpInput">
               <input
                 type="text"
                 name="account"
+                className="inputId"
                 onChange={this.handleOnchange}
               />
               <p>
@@ -76,11 +145,15 @@ class Signup extends Component {
             </div>
           </div>
           <div className="signUpItemBox">
-            <div className="signUpItem">비밀번호*</div>
+            <div className="signUpItem">
+              비밀번호
+              <span className="nessaryIcon">*</span>
+            </div>
             <div className="signUpInput">
               <input
                 type="password"
                 name="password"
+                className="inputPw"
                 onChange={handleOnchange}
               />
               <p>
@@ -90,28 +163,44 @@ class Signup extends Component {
             </div>
           </div>
           <div className="signUpItemBox">
-            <div className="signUpItem">비밀번호 확인*</div>
+            <div className="signUpItem">
+              비밀번호 확인
+              <span className="nessaryIcon">*</span>
+            </div>
             <div className="signUpInput">
-              <input type="password" name="pwCheck" onChange={handleOnchange} />
+              <input
+                type="password"
+                name="pwCheck"
+                className="inputPw"
+                onChange={handleOnchange}
+              />
             </div>
           </div>
           <div className="signUpItemBox">
-            <div className="signUpItem">이름*</div>
+            <div className="signUpItem">
+              이름
+              <span className="nessaryIcon">*</span>
+            </div>
             <div className="signUpInput">
               <input
                 type="text"
                 name="name"
+                className="inputName"
                 placeholder="이름(실명입력)"
                 onChange={handleOnchange}
               />
             </div>
           </div>
           <div className="signUpItemBox">
-            <div className="signUpItem">생년월일*</div>
+            <div className="signUpItem">
+              생년월일
+              <span className="nessaryIcon">*</span>
+            </div>
             <div className="signUpInput flexBox">
               <input
                 type="text"
                 name="year"
+                className="inputBirth"
                 placeholder="생년(4자)"
                 maxLength="4"
                 onChange={handleOnchange}
@@ -119,6 +208,7 @@ class Signup extends Component {
               <input
                 type="text"
                 name="month"
+                className="inputBirth"
                 placeholder="월"
                 maxLength="2"
                 onChange={handleOnchange}
@@ -126,6 +216,7 @@ class Signup extends Component {
               <input
                 type="text"
                 name="day"
+                className="inputBirth"
                 placeholder="일"
                 maxLength="2"
                 onChange={handleOnchange}
@@ -133,31 +224,50 @@ class Signup extends Component {
             </div>
           </div>
           <div className="signUpItemBox">
-            <div className="signUpItem">이메일*</div>
+            <div className="signUpItem">
+              이메일
+              <span className="nessaryIcon">*</span>
+            </div>
             <div className="signUpInput flexBox">
-              <input type="text" name="email" onChange={handleOnchange} />
-              @
-              <input type="text" name="emailDot" onChange={handleOnchange} />
+              <input
+                type="text"
+                name="email"
+                className="inputEmail"
+                onChange={handleOnchange}
+              />
+              <span className="inputMargin">@</span>
+              <input
+                type="text"
+                name="emailDot"
+                className="inputEmail"
+                onChange={handleOnchange}
+              />
             </div>
           </div>
           <div className="signUpItemBox">
-            <div className="signUpItem">휴대전화*</div>
+            <div className="signUpItem">
+              휴대전화
+              <span className="nessaryIcon">*</span>
+            </div>
             <div className="signUpInput flexBox">
               <input
+                className="inputPhone"
                 type="text"
                 maxLength="3"
                 onChange={handleOnchange}
                 name="phoneNum1"
               />
-              -
+              <span className="inputMargin">-</span>
               <input
+                className="inputPhone"
                 type="text"
                 maxLength="4"
                 onChange={handleOnchange}
                 name="phoneNum2"
               />
-              -
+              <span className="inputMargin">-</span>
               <input
+                className="inputPhone"
                 type="text"
                 maxLength="4"
                 onChange={handleOnchange}
@@ -165,8 +275,29 @@ class Signup extends Component {
               />
             </div>
           </div>
-          <button>확인</button>
         </form>
+        <div className="buttonWrapper">
+          <div className="buttonBox">
+            <button className="cancleButton">취소</button>
+            <button className="signUpButton" onClick={this.goTO}>
+              확인
+            </button>
+          </div>
+        </div>
+        <div className="premiumInfo">
+          <strong>
+            F&F membership은 회원님의 소중한 개인정보를 안전하게 관리하고
+            있습니다.
+          </strong>
+          <p>
+            2017년 8월 18일 부터 시행되는 정보통신망 이용 촉진 및 정보보호 등에
+            관한 법률(이하 정보통신망법) 제 23조의 2 ‘주민등록번호의 사용
+            제한’에 따라 모든 서비스에서 주민등록번호를 입력 받지 않습니다. 이에
+            따라 가입하실 때는 주민등록번호를 입력하는 실명인증 대신 아이핀
+            인증이나 휴대폰, 신용카드, 공인인증서를 이용하여 본인인증 하셔야
+            합니다.
+          </p>
+        </div>
       </div>
     );
   }
