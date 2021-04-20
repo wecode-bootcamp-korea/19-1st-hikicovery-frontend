89b13f380bbcaf5d6e7e1000e737c9e0461f3a6d
Juyoung Son <wndud0647@gmail.com>
2021-04-20 15:48:49 +0900

    Modify : css수정

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import LoginForm from './LoginForm/LoginForm';
 import './Login.scss';
 
 class Login extends Component {
@@ -23,45 +23,7 @@ class Login extends Component {
       <article className="loginContainer">
         <section className="loginContents">
           <div className="loginWrap">
-            <form className="loginForm">
-              <div className="chkMemberBox">
-                <div className="loginMember">회원</div>
-                <div className="loginGuest">비회원(주문조회)</div>
-              </div>
-              <input
-                className="inputId"
-                name="inputId"
-                type="text"
-                placeholder="아이디"
-                onChange={inputOnChange}
-              />
-              <input
-                className="inputPw"
-                name="inputPw"
-                type="password"
-                placeholder="비밀번호"
-                onChange={inputOnChange}
-              />
-              <input className="saveId" type="checkbox" />
-              아이디 저장
-              <div className="loginBtnBox">
-                <button className="loginBtn" onClick={chkLogin}>
-                  로그인
-                </button>
-                <button className="loginNBtn">네이버 아이디 로그인</button>
-              </div>
-              <div className="loginLinkBox">
-                <Link to="" className="findId">
-                  아이디 찾기
-                </Link>
-                <Link to="" className="findPw">
-                  비밀번호 찾기
-                </Link>
-                <Link to="/singup" className="singUp">
-                  회원가입
-                </Link>
-              </div>
-            </form>
+            <LoginForm inputOnChange={inputOnChange} chkLogin={chkLogin} />
           </div>
         </section>
       </article>
