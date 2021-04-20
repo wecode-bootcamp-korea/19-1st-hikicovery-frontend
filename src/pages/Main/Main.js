3f7053d03f862141d548496f564653232b090600
Juyoung Son <wndud0647@gmail.com>
2021-04-20 16:23:05 +0900

    Modify : 슬라이드

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -18,41 +18,67 @@ class Main extends Component {
         url:
           'https://static.discovery-expedition.com/images/display/category/THM/A01/A02/contents/88_5058_103_KOR_20210324181115.jpg',
       },
+      {
+        id: 3,
+        url:
+          'https://static.discovery-expedition.com/images/display/category/THM/A01/A02/contents/88_5058_97_KOR_20210415112127.png',
+      },
+      {
+        id: 4,
+        url:
+          'https://static.discovery-expedition.com/images/display/category/THM/A01/A02/contents/88_5058_107_KOR_20210413173838.jpg',
+      },
+      {
+        id: 5,
+        url:
+          'https://static.discovery-expedition.com/images/display/category/THM/A01/A02/contents/88_5058_101_KOR_20210319162531.jpg',
+      },
+      {
+        id: 6,
+        url:
+          'https://static.discovery-expedition.com/images/display/category/THM/A01/A02/contents/88_5058_108_KOR_20210415095117.png',
+      },
+      {
+        id: 7,
+        url:
+          'https://static.discovery-expedition.com/images/display/category/THM/A01/A02/contents/88_5058_105_KOR_20210401170117.jpg',
+      },
     ],
   };
 
-  prev = () => {};
-
-  next = () => {
-    const { imgIndex } = this.state;
+  changeMainImg = num => {
+    const { imgIndex, mainBackImgList } = this.state;
     this.setState({
-      imgIndex: imgIndex + 1,
+      ...imgIndex,
+      imgIndex:
+        imgIndex > mainBackImgList.length - 2 || imgIndex < 0
+          ? 0
+          : imgIndex + num,
     });
+    console.log(imgIndex);
+    console.log(mainBackImgList.length);
   };
 
   render() {
     const { mainBackImgList, imgIndex } = this.state;
-    const { prev, next } = this;
+    const { changeMainImg } = this;
     return (
       <div className="main">
         <div className="mainBackImgWrapper">
           <img
             src={mainBackImgList[imgIndex].url}
             alt="메인배경이미지"
-            className=""
+            className="mainBackImg"
           />
         </div>
         <div className="mainSlideController">
-          <button onClick={prev} className="mainPrevButton">
+          <button onClick={() => changeMainImg(-1)} className="mainPrevButton">
             <FaArrowLeft></FaArrowLeft>
           </button>
-          <button onClick={next} className="mainNextButton">
+          <button onClick={() => changeMainImg(1)} className="mainNextButton">
             <FaArrowRight></FaArrowRight>
           </button>
         </div>
-
-        <Nav />
-        <FixMenuBar />
       </div>
     );
   }
